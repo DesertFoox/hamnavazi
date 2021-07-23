9be89e720fd8a0da36689b88dc7e7df03b03f748
mohsen848esf <78562085+mohsen848esf@users.noreply.github.com>
2021-07-23 18:23:10 +0430

    landing top baner is done

--- src/Screens/Landing.jsx
+++ src/Screens/Landing.jsx
@@ -15,6 +15,58 @@ const Landing = () => {
   return (
     <React.Fragment>
       <div className="top-landing h-60">
+        {/* <div className="d-flex justify-content-end p-0 ">
+        </div> */}
+        <div className={`baner-box m-0`}>
+          <div
+            className={`baner-box-item baner-musice-box d-flex justify-content-between`}
+          >
+            <div className={`text-white px-3 py-1`}>
+              <h6>موسیـقی‌دان‌هـا</h6>
+              <h6>Musicians</h6>
+            </div>
+            <img
+              className={`img-fluid `}
+              src={
+                require("../Assets/Images/header/favorite-baner.png").default
+              }
+              alt=""
+            />
+          </div>
+
+          <div
+            className={`baner-box-item baner-service-box d-flex justify-content-between`}
+          >
+            <div className={`text-white px-3 py-1`}>
+              <h6>پیـراموسیـقی</h6>
+              <h6>Service Providers</h6>
+            </div>
+            <img
+              className={`img-fluid`}
+              src={
+                require("../Assets/Images/header/favorite-baner.png").default
+              }
+              alt=""
+            />
+          </div>
+
+          <div
+            className={`baner-box-item baner-bands-box d-flex justify-content-between`}
+          >
+            <div className={`text-white px-3 py-1`}>
+              <h6>گــروه‌هـا</h6>
+              <h6>Bands</h6>
+            </div>
+            <img
+              className={`img-fluid`}
+              src={
+                require("../Assets/Images/header/favorite-baner.png").default
+              }
+              alt=""
+            />
+          </div>
+        </div>
+
         <section className="landingtopsection container">
           <div className="grid justify-items-center ">
             <h2 className="text-white mt-xl">اینجا زبان نخست موسیقی است !</h2>
