3cf01e0fa0663f8dc12fe2eed476dec0d1115f9b
mohsen848esf <78562085+mohsen848esf@users.noreply.github.com>
2021-07-23 14:35:48 +0430

    fix conflict profile

--- src/Screens/Profile.jsx
+++ src/Screens/Profile.jsx
@@ -1,69 +1,69 @@
-import React, { useState, useEffect } from "react";
-import ProfileSideNave from "./Profile/SideNav/ProfileSideNave";
-import style from "./Profile/SideNav/ProfileSideNave.module.css";
-import "../Assets/Style/profile.css";
-import TabsNave from "./Profile/Tabs/TabsNaves/TabsNave";
-const Profile = () => {
-  const [windowWidth, setwindowWidth] = useState(null);
-  const [sidebarOpen, setsidebarOpen] = useState(true);
+// import React, { useState, useEffect } from "react";
+// import ProfileSideNave from "./Profile/SideNav/ProfileSideNave";
+// import style from "./Profile/SideNav/ProfileSideNave.module.css";
+// import "../Assets/Style/profile.css";
+// import TabsNave from "./Profile/Tabs/TabsNaves/TabsNave";
+// const Profile = () => {
+//   const [windowWidth, setwindowWidth] = useState(null);
+//   const [sidebarOpen, setsidebarOpen] = useState(true);
 
-  // if (sidebarOpen && windowWidth < 990) {
-  //   setsidebarOpen(false);
-  // } else if (windowWidth > 990) {
-  //   setsidebarOpen(true);
-  // }
+//   // if (sidebarOpen && windowWidth < 990) {
+//   //   setsidebarOpen(false);
+//   // } else if (windowWidth > 990) {
+//   //   setsidebarOpen(true);
+//   // }
 
-  const updateWidth = () => {
-    setwindowWidth(window.innerWidth);
-    console.log(windowWidth);
-  };
-  useEffect(() => {
-    if (windowWidth !== undefined) {
-      updateWidth();
-      window.addEventListener("resize", updateWidth);
-    }
-  }, []);
-  // const resizeStyle =
-  //   windowWidth &&
-  //   ((windowWidth < 769
-  //     ? style["profile-sideNave"] + " " + "col-6"
-  //     : "col-4") ||
-  //     (windowWidth < 576
-  //       ? style["profile-sideNave"] + " " + "col-6"
-  //       : "col-4"));
-  const resizeStyle2 = () => {
-    if (windowWidth > 990) {
-      return "col-4";
-    } else if (windowWidth <= 990) {
-      return style["profile-sideNave"] + " " + "col-10";
-    } else if (windowWidth < 576) {
-      return style["profile-sideNave"] + " " + "col-8";
-    } else if (windowWidth < 470) {
-      return style["profile-sideNave"] + " " + "col-10";
-    } else if (windowWidth < 375) {
-      return style["profile-sideNave"] + " " + "col-10";
-    }
-  };
-  return (
-    <section className="profile container-fluid profile-bg">
-      <div className={`${style[""]} row h-100`}>
-        <div
-          className={`${resizeStyle2()} SideNave  bg-white p-0`}
-          style={sidebarOpen ? { display: "grid" } : { display: "none" }}
-        >
-          <ProfileSideNave isOpen={setsidebarOpen} />
-        </div>
-        <div className="col-lg-8 bg-selectbrown p-0">
-          <TabsNave screenWidth={windowWidth} isOpen={setsidebarOpen} />
-          222
-          <div className="row">
-            <div className="col-lg-6 bg-white"></div>
-            <div className="col-lg-6 bg-white"></div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
+//   const updateWidth = () => {
+//     setwindowWidth(window.innerWidth);
+//     console.log(windowWidth);
+//   };
+//   useEffect(() => {
+//     if (windowWidth !== undefined) {
+//       updateWidth();
+//       window.addEventListener("resize", updateWidth);
+//     }
+//   }, []);
+//   // const resizeStyle =
+//   //   windowWidth &&
+//   //   ((windowWidth < 769
+//   //     ? style["profile-sideNave"] + " " + "col-6"
+//   //     : "col-4") ||
+//   //     (windowWidth < 576
+//   //       ? style["profile-sideNave"] + " " + "col-6"
+//   //       : "col-4"));
+//   const resizeStyle2 = () => {
+//     if (windowWidth > 990) {
+//       return "col-4";
+//     } else if (windowWidth <= 990) {
+//       return style["profile-sideNave"] + " " + "col-10";
+//     } else if (windowWidth < 576) {
+//       return style["profile-sideNave"] + " " + "col-8";
+//     } else if (windowWidth < 470) {
+//       return style["profile-sideNave"] + " " + "col-10";
+//     } else if (windowWidth < 375) {
+//       return style["profile-sideNave"] + " " + "col-10";
+//     }
+//   };
+//   return (
+//     <section className="profile container-fluid profile-bg">
+//       <div className={`${style[""]} row h-100`}>
+//         <div
+//           className={`${resizeStyle2()} SideNave  bg-white p-0`}
+//           style={sidebarOpen ? { display: "grid" } : { display: "none" }}
+//         >
+//           <ProfileSideNave isOpen={setsidebarOpen} />
+//         </div>
+//         <div className="col-lg-8 bg-selectbrown p-0">
+//           <TabsNave screenWidth={windowWidth} isOpen={setsidebarOpen} />
+//           222
+//           <div className="row">
+//             <div className="col-lg-6 bg-white"></div>
+//             <div className="col-lg-6 bg-white"></div>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
 
-export default Profile;
+// export default Profile;
--- src/Screens/Profile/Profile/Profile.jsx
+++ src/Screens/Profile/Profile/Profile.jsx
@@ -1,108 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Badge } from "reactstrap";
 
 import ProfileSideNave from "../SideNav/ProfileSideNave";
-
+import TabsNave from "../Tabs/TabsNaves/TabsNave";
+import style from "../SideNav/ProfileSideNave.module.css";
+import "../../../Assets/Style/profile.css";
 const Profile = () => {
+  const [windowWidth, setwindowWidth] = useState(null);
+  const [sidebarOpen, setsidebarOpen] = useState(true);
+
+  // if (sidebarOpen && windowWidth < 990) {
+  //   setsidebarOpen(false);
+  // } else if (windowWidth > 990) {
+  //   setsidebarOpen(true);
+  // }
+
+  const updateWidth = () => {
+    setwindowWidth(window.innerWidth);
+    console.log(windowWidth);
+  };
+  useEffect(() => {
+    if (windowWidth !== undefined) {
+      updateWidth();
+      window.addEventListener("resize", updateWidth);
+    }
+  }, []);
+  // const resizeStyle =
+  //   windowWidth &&
+  //   ((windowWidth < 769
+  //     ? style["profile-sideNave"] + " " + "col-6"
+  //     : "col-4") ||
+  //     (windowWidth < 576
+  //       ? style["profile-sideNave"] + " " + "col-6"
+  //       : "col-4"));
+  const resizeStyle2 = () => {
+    if (windowWidth > 990) {
+      return "col-4";
+    } else if (windowWidth <= 990) {
+      return style["profile-sideNave"] + " " + "col-10";
+    } else if (windowWidth < 576) {
+      return style["profile-sideNave"] + " " + "col-8";
+    } else if (windowWidth < 470) {
+      return style["profile-sideNave"] + " " + "col-10";
+    } else if (windowWidth < 375) {
+      return style["profile-sideNave"] + " " + "col-10";
+    }
+  };
   return (
     <section className="profile container-fluid profile-bg">
-      <div className="row">
-        <div className="col-lg-4 d-flex justify-content-start bg-white p-0">
-          <ProfileSideNave />
+      <div className={`${style[""]} row h-100`}>
+        {" "}
+        <div
+          className={`${resizeStyle2()} SideNave  bg-white p-0`}
+          style={sidebarOpen ? { display: "grid" } : { display: "none" }}
+        >
+          <ProfileSideNave isOpen={setsidebarOpen} />
         </div>
-        <div className="col-lg-8 bg-selectbrown">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-3">
-            <div className="mb-sm ml-ssm py-4 px-5 rounded bg-white shadow-lg">
-              <div className="name mb-2rm">
-                <p className="text-textcolor text-13">نام | Name</p>
-                <p className="text-13">عارف طلوعی</p>
-              </div>
-              <div className="username mb-2rm">
-                <p className="text-textcolor text-13">نام کاربری | Username</p>
-                <p className="text-black text-13">Aref Too</p>
-              </div>
-              <div className="gender mb-2rm">
-                <p className="text-textcolor text-13"> جنسیت | Gender</p>
-                <p className="text-black text-13">مرد </p>
-              </div>
-              <div className="birthdate mb-2rm">
-                <p className="text-textcolor text-13">
-                  {" "}
-                  تاریخ تولد | Date Of Birth
-                </p>
-                <p className="text-black text-13">1983-25-02 </p>
-              </div>
-              <div className="languages mb-2rm">
-                <p className="text-textcolor text-13">
-                  تاریخ تولد | Languages{" "}
-                </p>
-                <p className="text-black text-13">فارس | انگلیسی | عربی</p>
-              </div>
-              <div className="location mb-2rm">
-                <p className="text-textcolor text-13">
-                  {" "}
-                  موقعیت مکانی | Location{" "}
-                </p>
-                <p className="text-black text-13">ایران | تهران | هفت تیر</p>
-              </div>
-            </div>
-            <div className="mb-sm ml-ssm py-4 px-5 rounded bg-white shadow-lg">
-              <div className="email">
-                <p className="text-textcolor text-13">ایمیل | Email</p>
-                <p className="text-black text-13">
-                  Email | پست الکترونیک areftoloei@gmail.com
-                </p>
-              </div>
-              <div className="website">
-                <p className="text-textcolor text-13">وبسایت | Website</p>
-                <p className="text-black text-13">www.chavoosh.info</p>
-              </div>
-              <div className="links">
-                <p className="text-textcolor text-13">
-                  لینک های ارتباطی | Links
-                </p>
-                <p className="text-black text-13">@areftoo</p>
-              </div>
-              <div className="links">
-                <p className="text-textcolor text-13">تلفن همراه | Cell</p>
-                <p className="text-black text-13">09123235676</p>
-              </div>
-              <div className="secgender">
-                <p className="text-textcolor text-13">جنسیت | Gender</p>
-                <p className="text-black text-13">مرد</p>
-              </div>
-            </div>
-          </div>
-          <div className="grid grid-cols-1 gap-4  gap-4 p-3">
-            <div className="instruments grid grid-cols-1 md:grid-cols-2 gap 4 mb-sm ml-ssm py-4 px-5 rounded bg-white shadow-lg">
-              <div className="saz mb-ssm">
-                <p className="text-textcolor text-13">
-                  ساز اصلی | Main instruments{" "}
-                  <Badge className="bg-572a47 rounded-full">
-                    حرفه ای | Pro
-                  </Badge>
-                </p>
-                <p className="text-black text-13"> _تار | Tar</p>
-                <p className="text-black text-13">_1988 | 1366</p>
-                <p className="text-black text-13">
-                  _بداهه نوازی | Improvisation
-                </p>
-                <p className="text-black text-13">_کلاسیک | Classic</p>
-                <p className="text-black text-13">_تلفیقی |Fusion</p>
-              </div>
-              <div className="saz-2">
-                <p className="text-textcolor text-13">
-                  ساز دوم | Second instruments
-                  <Badge className="rounded-full bg-gold">
-                     مبتدی | begginer
-                  </Badge>
-                </p>
-                <p className="text-black text-13"> _طبلا | Tabla</p>
-                <p className="text-black text-13">_1988 | 1366</p>
-                <p className="text-black text-13">_هندوستامی | Hindustani</p>
-              </div>
-            </div>
-          </div>
+        <div className="col-lg-8 bg-selectbrown p-0">
+          <TabsNave screenWidth={windowWidth} isOpen={setsidebarOpen} />
         </div>
       </div>
     </section>
