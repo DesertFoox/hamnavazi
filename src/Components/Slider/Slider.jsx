836528c89875768acfc38003dbb962b4a6e70ed1
mohsen848esf <78562085+mohsen848esf@users.noreply.github.com>
2021-07-28 12:28:00 +0430

    style to slider arrow

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { ChevronLeft, ChevronRight } from "react-feather";
 import Slider from "react-slick";
+
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
-import '../../Assets/Style/slider.scss'
+import "../../Assets/Style/slider.scss";
 const Slide = (props) => {
   const { buttonLabel, className } = props;
 
@@ -16,13 +18,21 @@ const Slide = (props) => {
     slidesToShow: 1,
     slidesToScroll: 1,
     initialSlide: 2,
+    nextArrow: <ChevronLeft size={40} className={`coustom-arrow`} />,
+    prevArrow: <ChevronRight size={40} />,
   };
   return (
     <div>
       <Button color="danger" onClick={toggle}>
         {buttonLabel}
       </Button>
-      <Modal centered color='danger' isOpen={modal} toggle={toggle} className={className + " modall"}>
+      <Modal
+        centered
+        color="danger"
+        isOpen={modal}
+        toggle={toggle}
+        className={className + " modall"}
+      >
         <ModalBody className="body">
           <div className="container">
             <Slider {...settings}>
